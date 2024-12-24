2ccab7784b153b7bb06f9bd7773ceceb2b2a712b
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-12-25 02:33:41 +0530

    mongodb bug resolution

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { swaggerConfig } from './config/swagger.config';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   const configService = app.get(ConfigService);
   app.enableCors();
 
