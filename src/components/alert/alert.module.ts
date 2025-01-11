7378e47ede45a15b760f3ca1334113be15f57f12
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-01-11 19:23:13 +0530

    AlertLog service initialization

--- src/components/alert/alert.module.ts
+++ src/components/alert/alert.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Alert, AlertSchema } from "./schemas/alert.schema";
 import { Event, EventSchema } from "./schemas/event.schema";
+import { AlertLog, AlertLogSchema } from "./schemas/alertLog.schema";
 import { PassportModule } from "@nestjs/passport";
 import { ConfigModule } from "@nestjs/config";
 import { AlertController } from "./alert.controller"; 
@@ -12,7 +13,8 @@ import { AlertService } from "./alert.service";
         MongooseModule.forFeature(
             [
                 { name: Alert.name, schema: AlertSchema },
-                { name: Event.name, schema: EventSchema }
+                { name: Event.name, schema: EventSchema },
+                { name: AlertLog.name, schema: AlertLogSchema },
             ]
         ),
         PassportModule,
