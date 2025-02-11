70a5022cd0822e97fd04daf794419cf5926fc559
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-02-12 00:20:01 +0530

    get alert logs service update

--- src/components/alert/dto/alertLog-response.dto.ts
+++ src/components/alert/dto/alertLog-response.dto.ts
@@ -1,15 +1,16 @@
 import { AlertCreationDto } from "./alert-creation.dto";
 import { AlertLogCreationDto } from "./alertLog-creation.dto";
+import { Date } from "mongoose";
 
 export class AlertLogResponseDto{
     title: string;
     classname: string;
     alert_type: string;
-    start_date: string;
-    end_date: string;
+    start_date: Date;
+    end_date: Date;
     start_time: string;
     end_time: string;
     status: string;
-    createdAt: string;
+    createdAt: Date;
     alertLogs: AlertLogCreationDto[]
 }
