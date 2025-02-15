83fb8973ff952a071bb352162f8d76b81adb9ac4
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-02-15 15:48:14 +0530

    custom-event service updat

--- src/components/custom-events/dto/customEvent-response.dto.ts
+++ src/components/custom-events/dto/customEvent-response.dto.ts
@@ -1,9 +1,11 @@
+import { Date } from "mongoose";
+
 export class CustomEventResponseDto {
     id: string;
     title: string;
     classname: string;
-    start_date: string;
-    end_date: string;
+    start_date: Date;
+    end_date: Date;
     start_time: string;
     end_time: string;
     status: string;
